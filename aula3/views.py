4cd1d8e9c9a2764b67c2a4a307875eb992a77925
Nathan Lusa <nathanlusa@gmail.com>
2014-08-19 23:13:57 -0300

    Ajustes

--- aula3/views.py
+++ aula3/views.py
@@ -2,8 +2,21 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def index(request):
-	response = HttpResponse()
-	response.write('Olá Mundo!')
-	return response
+	if request.method == 'POST':
+		nome = request.POST.get('name', 'não tem nome')
+		return HttpResponse('O nome é %s' % nome)
+	else:
+		formulario = '''
+		<form action="." method="post">
+		<input type="text" name="name" maxlenght="100" />
+		<button type="submit">Enviar</button>
+		</form>
+		'''
+		return HttpResponse(formulario)
+
+def detail(request, id):
+	return HttpResponse('O id é: %s' % id) 
