1123a3773d43e24213d5fc9b92b4b22bc907a18c
Nathan Lusa <nathanlusa@gmail.com>
2014-08-24 22:00:12 -0300

    Aula 5.1

--- aula5/models.py
+++ aula5/models.py
@@ -2,8 +2,9 @@
 from django.db import models
 
 # Create your models here.
-class contato(models.Model):
+class Contato(models.Model):
 	
 	nome = models.CharField(max_length=100)
 	email = models.EmailField()
 	twitter = models.URLField()
+	dtNascimento = models.DateField()
