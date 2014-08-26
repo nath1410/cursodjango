730f846bbbdcffd520fd5ac1a99104e70a326e90
Nathan Lusa <nathanlusa@gmail.com>
2014-08-26 14:29:52 -0300

    Aula 5.2

--- aula5/models.py
+++ aula5/models.py
@@ -8,3 +8,33 @@ class Contato(models.Model):
 	email = models.EmailField()
 	twitter = models.URLField()
 	dtNascimento = models.DateField()
+
+	def __unicode__(self):
+		return self.nome
+
+
+class Categoria(models.Model):
+
+	nome = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.nome
+
+
+class Post(models.Model):
+
+	titulo = models.CharField(max_length=200)
+	text = models.TextField(max_length=1024)
+	categorias = models.ManyToManyField(Categoria)
+
+	def __unicode__(self):
+		return self.titulo
+
+class Comentarios(models.Model):
+
+	autor = models.CharField(max_length=100)
+	comentario = models.TextField(max_length=1024)
+	post = models.ForeignKey(Post)
+
+	def __unicode__(self):
+		return self.autor
