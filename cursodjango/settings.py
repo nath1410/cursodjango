ef884b302a16bc630857aa891d8442d132fd2644
Nathan Lusa <nathanlusa@gmail.com>
2014-11-25 12:51:39 -0200

    1

--- cursodjango/settings.py
+++ cursodjango/settings.py
@@ -67,14 +67,14 @@ WSGI_APPLICATION = 'cursodjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cursodjango',
-        'USER': 'root',
-        'PASSWORD': 'root', 
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'cursodjango',
+        #'USER': 'root',
+        #'PASSWORD': 'root', 
+        #'HOST': '',
+        #'PORT': '',
     }
 }
 
--- cursodjango/urls.py
+++ cursodjango/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^aula3/$', 'aula3.views.index', name='aula3_index'),
-	url(r'^aula3/(?P<username>[\w-]+)/$', 'aula3.views.detail', name='aula3_detail'),
+	  url(r'^aula3/(?P<username>[\w-]+)/$', 'aula3.views.detail', name='aula3_detail'),
     url(r'^aula4/$', 'aula4.views.index', name='aula4_index'),
     url(r'^aula4/home/$', 'aula4.views.home', name='aula4_home'),
     url(r'^aula6/$', 'aula6.views.index', name='aula6_index'),
